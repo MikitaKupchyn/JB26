85aac87422c0dc897c26ef9a30b945ce20fab454
MikitaKupchyn <43317704+MikitaKupchyn@users.noreply.github.com>
2018-11-05 22:20:38 +0300

    Lesson9

---
+++ Lesson9/ArtisticBook.java
@@ -0,0 +1,19 @@
+package Lesson9;
+
+public class ArtisticBook extends Book{
+	
+	private String readability;
+
+	public ArtisticBook(String title, int publishYear, String author, String readability) {
+		super(title, publishYear, author);
+		this.readability = readability;
+	}
+
+	public String getReadability() {
+		return readability;
+	}
+
+	public void setReadability(String readability) {
+		this.readability = readability;
+	}
+}
---
+++ Lesson9/FantasticBook.java
@@ -0,0 +1,21 @@
+package Lesson9;
+
+public class FantasticBook extends Book{
+	
+	private String genre;
+
+	public FantasticBook(String title, int publishYear, String author, String genre) {
+		super(title, publishYear, author);
+		this.genre = genre;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
+}
