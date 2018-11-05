4e66c7e5be3abe1b1d925b676e648d637c1eb403
MikitaKupchyn <43317704+MikitaKupchyn@users.noreply.github.com>
2018-11-05 22:19:57 +0300

    Lesson6
    Lesson6

---
+++ lesson6/main/Group.java
@@ -0,0 +1,33 @@
+package by.epam.lesson6.main;
+
+public class Group {
+	
+	private Student[] students;
+	private double averageMarkOfGroup;
+	private int sizeOfGroup = 0;
+
+	public Group(int sizeOfGroup) {
+		students = new Student[sizeOfGroup];
+	}
+
+	public void add(Student student) {
+		students[sizeOfGroup] = student;
+		sizeOfGroup++;
+	}
+
+	public double getAverageMarkOfGroup() {
+		int sumOfAverageMarkOfStudents = 0;
+		for (int i = 0; i < students.length; i++) {
+			sumOfAverageMarkOfStudents += students[i].getAvrMark();
+		}
+		averageMarkOfGroup = sumOfAverageMarkOfStudents / sizeOfGroup;
+		return averageMarkOfGroup;
+	}
+
+	public void printAllStudent() {
+		for (int i = 0; i < students.length; i++) {
+			System.out.println(students[i].toString());
+		}
+	}
+
+}
