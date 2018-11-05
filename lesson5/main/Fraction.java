ddcbcbb14cc8187ce8a63a20a266c09d4469c67c
MikitaKupchyn <43317704+MikitaKupchyn@users.noreply.github.com>
2018-11-05 22:19:21 +0300

    Lesson4
    Lesson4

---
+++ lesson5/main/Fraction.java
@@ -0,0 +1,20 @@
+package by.epam.lesson5.main;
+
+public class Fraction {
+	
+		private double m;
+		private double n;
+
+		public Fraction(int m, int n) {
+			this.m = m;
+			this.n = n;
+		}
+
+		public double getFraction() {
+			return m / n;
+		}
+
+	}
+	
+
+
